bcfa7bd9c2d502ef377d5bbfa18032190d7cc57d
Carson Full <carson_full@tsco.org>
2022-11-29 15:58:42 -0600

    chore: fix type from TS 4.9

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ export type MaybeAsync<T> = T | Promise<T>;
 export type Many<T> = T | readonly T[];
 
 export const many = <T>(item: Many<T>): readonly T[] =>
-  Array.isArray(item) ? item : [item];
+  Array.isArray(item) ? item : [item as T];
 
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
